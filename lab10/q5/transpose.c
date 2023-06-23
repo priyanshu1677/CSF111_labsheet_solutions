1630abaafcd30d8447ef2491b1b3acf645fd22d8
Priyanshu1677 <jainpriyanshu1677@gmail.com>
2023-06-24 04:04:56 +0530

    bugs removed

--- lab10/q5/transpose.c
+++ lab10/q5/transpose.c
@@ -2,15 +2,26 @@
 
 //implement this function as defined in transpose.h
 void transposeByReference(int matrix[4][4]){
-    for(int i=0;i<4;i++){
-        for(int j=0;j<4;j++){
-            matrix[i][j]=matrix[j][i];
+    for (int i = 0; i < 4; i++) {
+        for (int j = i + 1; j < 4; j++) {
+            // Swap elements at (i, j) and (j, i)
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
         }
     }
 }
 
-
 //implement this function as defined in transpose.h
 void transposeByPointer(int* matrix){
-    
+    for (int i = 0; i < 4; i++) {
+        for (int j = i + 1; j < 4; j++) {
+            // Swap elements at (i, j) and (j, i) using pointer arithmetic
+            int* element1 = matrix + i * 4 + j;
+            int* element2 = matrix + j * 4 + i;
+            int temp = *element1;
+            *element1 = *element2;
+            *element2 = temp;
+        }
+    }
 }

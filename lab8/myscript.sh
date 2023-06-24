e0aee2cb2a01e292be857f28a7967b07ab92c25f
Priyanshu1677 <jainpriyanshu1677@gmail.com>
2023-06-24 19:35:20 +0530

    changes in sh

--- lab8/myscript.sh
+++ lab8/myscript.sh
@@ -3,6 +3,9 @@ gcc -c student2.c
 gcc -c student3.c
 gcc -c checkAnswers.c
 
+
 gcc -o exe1 student1.o checkAnswers.o 
 gcc -o exe2 student2.o checkAnswers.o
 gcc -o exe3 student3.o checkAnswers.o
+
+rm *.o
